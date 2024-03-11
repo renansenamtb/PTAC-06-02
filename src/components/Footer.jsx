651f940a43d94edb0fa7d0406c2836827d7cb11d
renansenamtb <renansenazzz@gmail.com>
2024-03-10 21:21:12 -0300

    ptacc
    concluido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 export default function Footer(){
     return(
         <footer>
-            <p>
-                
-                Desenvolvedor:Renan
-</p>
+          
+            <p>Desenvolvedor:Renan Sena</p>    
+
+            
+        
+            
+           
         </footer>
     );
 }
